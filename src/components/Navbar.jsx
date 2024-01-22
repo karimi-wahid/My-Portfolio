cf559d3327fb4757257bb38e0972f233f635806a
wahidullah karimi <wahidullah.karimi22@gmail.com>
2024-01-22 11:23:16 +0430

    Navbar has been completed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,69 @@
+import React,{ useState } from 'react'
+import {FaBars, FaTimes, FaGithub,FaFacebook, FaLinkedin} from 'react-icons/fa';
+import{HiOutlineMail} from 'react-icons/hi';
+import {BsFillPersonLineFill, BsFillPersonLinesFill} from 'react-icons/bs'
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+     
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        <div>
+            <h1 style={{width:'50px'}} alt="logo">Karimi</h1>
+        </div>
+
+        {/* menu */}
+        <ul className='hidden md:flex'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Work</li>
+            <li>Contact</li>
+        </ul>
+
+        {/* Hamburger */}
+        <div onClick={handleClick} className='md:hidden z-10'>
+            {!nav ? <FaBars /> : <FaTimes />}
+        </div>
+
+        {/* Mobile Menu */}
+        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+            <li className='py-6 text-4xl  :hover-'>Home</li>
+            <li className='py-6 text-4xl  :hover-'>About</li>
+            <li className='py-6 text-4xl :hover-'>Skills</li>
+            <li className='py-6 text-4xl :hover-'>Work</li>
+            <li className='py-6 text-4xl :hover-'>Contact</li>
+        </ul>
+
+        {/* Social icons */}
+        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+            <ul>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-500 bg-blue-700'>
+                    <a className='flex justify-between items-center w-full text-gray-300'
+                    href='#' >Facebook <FaFacebook size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-500 bg-[#333333]'>
+                    <a className='flex justify-between items-center w-full text-gray-300'
+                    href='#' >Github <FaGithub size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-500 bg-[#6fc2b0]'>
+                    <a className='flex justify-between items-center w-full text-gray-300'
+                    href='#' >Email <HiOutlineMail size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-500 bg-[#565f69]'>
+                    <a className='flex justify-between items-center w-full text-gray-300'
+                    href='#' >Resume <BsFillPersonLinesFill size={30}/>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
