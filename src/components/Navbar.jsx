1014493627d770af36c7dbeb645384ddf3f92d09
wahidullah karimi <wahidullah.karimi22@gmail.com>
2024-01-24 11:36:09 +0430

    some changes add.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
-            <h1 style={{width:'50px'}} alt="logo">Karimi</h1>
+            <h1 className='text-2xl' style={{width:'50px'}} alt="logo">Karimi</h1>
         </div>
 
         {/* menu */}
